91c7621631d0febc5fc25db26b8371697ab80fa7
DenisChuvilin <denischuv@gmail.com>
2020-02-12 08:00:03 -0800

    got some stuff done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
-// Your code goes here
+const image = document.querySelectorAll('img');
+
+image.forEach(img => {
+  img.addEventListener('mouseover', () => {
+    img.style.transform = 'scale(1.1)';
+    img.style.transition = 'transform 1s';
+  });
+});
+
+image.forEach(img => {
+  img.addEventListener('mouseleave', () => {
+    img.style.transform = 'scale(1)';
+    img.style.transition = 'transform 1s';
+  });
+});
+
+const text = document.querySelectorAll('p');
+text.forEach(txt =>
+  window.addEventListener('scroll', () => {
+    txt.style.color = 'orange';
+  })
+);
+
+const removeTxt = document.querySelectorAll('p');
+
+removeTxt.forEach(txt =>
+  txt.addEventListener('select', () => {
+    txt.textContent = '';
+  })
+);
